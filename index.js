13c5deb36d466dc11ea711de86351efcd167d6ad
Hugo Antunes <hugoalexantunes@gmail.com>
2023-02-10 17:24:11 +0000

    Init code

---
+++ index.js
@@ -0,0 +1,14 @@
+import "./styles.css";
+
+// declare the variables
+var x = 5;
+var y = 6;
+// Add the two variables
+var z = x + y;
+
+document.getElementById("app").innerHTML = `
+<h1>JavaScript Variables!</h1>
+<div>
+ The result of z (x+y) is: ${z}
+</div>
+`;
